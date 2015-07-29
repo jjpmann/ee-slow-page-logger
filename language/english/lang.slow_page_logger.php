851d18ad8d6a113c5851af026d20961a45d4c6cb
Jerry Price <jerryprice@lmo.com>
2015-07-28 22:29:52 -0400

    adding lang file

---
+++ language/english/lang.slow_page_logger.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang = array(
+  'execution_time'  => 'Execution Time in seconds.',
+  'memory_usage'    => 'Memory Size in bytes.',
+  'total_queries'   => 'Total Queries used to render the page.'
+);
